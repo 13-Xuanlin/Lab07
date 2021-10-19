9c09f300c25beda96e3ed293d667aadf963a45a5
13-Xuanlin <xuanlin56@hotmail.com>
2021-10-19 21:09:34 +0800

    Added background images and updated backgroundscript

---
+++ Assets/Scripts/BackGroundChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackGroundChange : MonoBehaviour
+{
+    public Image image;
+    public Sprite[] Sprites;
+    private int y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        y = Random.Range(0, 3);
+        image.sprite = Sprites[y];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
