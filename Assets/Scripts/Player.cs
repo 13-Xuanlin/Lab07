b2022cd607e8d2c9c24cd19ad537b7bde67d4400
13-Xuanlin <xuanlin56@hotmail.com>
2021-10-19 15:59:15 +0800

    Update Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public float Speed;
 
     void Start()
     {
@@ -14,7 +16,23 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (transform.position.y < -5)
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+        if (transform.position.y <= 3.5)
+        {
+            transform.position += new UnityEngine.Vector3(0, 0, Time.deltaTime * Speed);
+        }
         if (Input.GetKeyDown(KeyCode.Space))
             thisAnimation.Play();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+            
+        }
     }
 }
